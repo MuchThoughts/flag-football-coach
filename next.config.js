6133f481852868a49daaaee64a94e4189171faa7
Sean <seanmichaeldavidson@gmail.com>
2026-06-13 00:05:44 -0500

    Fix Vercel tracing root

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '..')
+  outputFileTracingRoot: __dirname
 }
 
 module.exports = nextConfig
